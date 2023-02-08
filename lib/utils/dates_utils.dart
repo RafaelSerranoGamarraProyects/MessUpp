74540a37d06b510c7f6f4b241dda35e53a5d6069
RafaSG13 <rafasrrg13@gmail.com>
2023-02-08 19:35:28 +0100

    datesRefactoring for translated months

---
+++ lib/utils/dates_utils.dart
@@ -0,0 +1,17 @@
+
+class DateRefactoring{
+	static Map<String, String> months =	{
+		"Jan" : "Enero",
+		"Feb" : "Febrero",
+		"Mar" : "Marzo",
+		"Apr" : "Abril",
+		"May" : "Mayo",
+		"Jun" : "Junio",
+		"Jul" : "Julio",
+		"Aug" : "Agosto",
+		"Sep" : "Septiembre",
+		"Oct" : "Octubre",
+		"Nov" : "Noviembre",
+		"Dec" : "Diciembre",
+	};
+}
