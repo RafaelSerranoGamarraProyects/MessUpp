0d51bd2e801a33c0e700919a8fb6ce9b597f5bba
Rafael Serrano Gamarra <rafasrrg13@gmail.com>
2024-04-18 12:52:17 +0200

    create group screen (#12)
    Co-authored-by: RafaSG13 <=>

--- lib/screens/groups_screen.dart
+++ lib/screens/groups_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:Messup/providers/providers.dart';
 import 'package:Messup/widgets/popup_add_group_payment.dart';
-import '../models/models.dart';
 import '../widgets/widgets.dart';
 
 class GroupScreen extends StatelessWidget {
@@ -45,31 +44,3 @@ class GroupScreen extends StatelessWidget {
 	}
 }
 
-class GroupItem extends StatefulWidget {
-	final Group userGroup;
-  const GroupItem({
-    super.key, required this.userGroup,
-  });
-
-  @override
-  State<GroupItem> createState() => _GroupItemState();
-}
-
-class _GroupItemState extends State<GroupItem> {
-  @override
-  Widget build(BuildContext context) {
-    return  Column(
-      children: [
-				Card(
-					color: Colors.white,
-					child: ListTile(
-						trailing: const Icon(Icons.keyboard_arrow_right_outlined,color: Colors.black, size: 30),
-						title: Text(widget.userGroup.name,style: const TextStyle(color: Colors.black, fontSize: 20),),
-						onTap: () => Navigator.pushReplacementNamed(context, 'group_details', arguments: widget.userGroup),
-						//leading: Container(height: 50,width: 50,decoration: const BoxDecoration(color: Colors.amber,shape: BoxShape.circle),child: Container(),)
-					),
-				)
-      ],
-    );
-  }
-}
